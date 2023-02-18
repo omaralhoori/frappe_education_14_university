3884a410914ac3c37f376594ea628a3e3adc25b6
omar alhori <omar@DESKTOP-GSCPV96.localdomain>
2023-02-18 23:57:05 +0300

    create personal info page

--- education/education/web_form/student_applicant/student_applicant.js
+++ education/education/web_form/student_applicant/student_applicant.js
@@ -1,3 +1,23 @@
+function reloadCurrentStateOptions() {
+	console.log('reloadCurrentStateOptions');
+   // frappe.web_form.set_value(['first_name'], ['{{username}}'])
+}
+
 frappe.ready(function() {
-	// bind events here
-})
+	{% if new_applicant %}
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	frappe.web_form.set_value(['program'], [urlParams.get('program')]);
+	
+	frappe.web_form.set_value(['first_name'], ['{{user.first_name}}']);
+	frappe.web_form.set_value(['middle_name'], ['{{user.middle_name}}']);
+	frappe.web_form.set_value(['last_name'], ['{{user.last_name}}']);
+	frappe.web_form.set_value(['student_email_id'], ['{{user.email}}']);
+	frappe.web_form.set_value(['student_mobile_number'], ['{{user.mobile_no}}']);
+
+	frappe.web_form.set_value(['academic_term'], ['{{student_admission.academic_term}}']);
+	frappe.web_form.set_value(['academic_year'], ['{{student_admission.academic_year}}'])
+	
+	console.log('{{student_admission.academic_term}}')
+	{% endif %}
+});
--- education/education/web_form/student_applicant/student_applicant.py
+++ education/education/web_form/student_applicant/student_applicant.py
@@ -1,6 +1,17 @@
 import frappe
-
+from frappe import _
 
 def get_context(context):
-	# do your magic here
-	pass
+	if not context.is_new: return
+	# student_admission
+	student_admission = frappe.local.form_dict.student_admission
+	if not student_admission: frappe.throw(_('Unable to find student admission'))
+	if not frappe.db.exists('Student Admission', student_admission): frappe.throw(_('Unable to find student admission'))
+	student_admission = frappe.get_doc("Student Admission", student_admission)
+	context.student_admission= student_admission
+	
+	# user
+	user = frappe.get_doc('User', frappe.session.user)
+	context.user = user
+
+	context.new_applicant= True
